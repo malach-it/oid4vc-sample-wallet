a3b8d5c95f2fa48376989fa737b3b5a607b8455d
Pascal Knoth <pascal@malach.it>
2024-04-18 10:56:45 +0200

    add scope to authorization link

--- src/services/oauth.service.ts
+++ src/services/oauth.service.ts
@@ -20,7 +20,8 @@ class Oauth {
     this.googleClient = new oauth.PreauthorizedCode({
       clientId: '26e571cc-31d5-4f5a-bba3-9151f2714fca',
       redirectUri: 'https://oid4vc-sample-wallet.boruta.patatoid.fr/oauth-callback',
-      clientSecret: ''
+      clientSecret: '',
+      scope: 'FederatedBorutaCredentialSdJwt'
     })
     this.githubClient = new oauth.PreauthorizedCode({
       clientId: '955e6646-a7a8-4c2b-80cb-7a9e12d41dfb',
